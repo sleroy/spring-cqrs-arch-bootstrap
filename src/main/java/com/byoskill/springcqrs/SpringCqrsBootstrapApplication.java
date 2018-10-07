4030dcc0510b557009c2df98f1e2c467b9dcbd20
Sylvain Leroy <sleroy@byoskill.com>
2018-10-07 16:36:53 +0200

    Demo project to start with CQRS

---
+++ src/main/java/com/byoskill/springcqrs/SpringCqrsBootstrapApplication.java
@@ -0,0 +1,12 @@
+package com.byoskill.springcqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCqrsBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCqrsBootstrapApplication.class, args);
+	}
+}
